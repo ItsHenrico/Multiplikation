bffb712a0a2d6c7e8b7c1d59cf0691ffffc8d4b5
ItsHenrico <186ocirneh@gmail.com>
2023-10-18 09:41:09 +0200

    sdaf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int i = 0;
+        Scanner sadf;
+        System.out.println("Vilket nummer?");
+        Scanner sc2 = new Scanner(System.in);
+        int input = sc2.nextInt();
+        while (i <= 10) {
+            int svar = input * i;
+            i++;
+            System.out.println(svar);
+        }
+        i = 0;
+        do {
+            int svar = input * i;
+            i++;
+            System.out.println(svar);
+        } while (i <= 10);
+        for (i = 0; i<=10; i++){
+            int svar = input * i;
+            System.out.println(svar);
+        }
     }
 }
